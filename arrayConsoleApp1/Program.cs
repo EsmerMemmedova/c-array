17ffee50506de37f67ea2b98cd75a02039ce50e6
Esmer <bd7tqjs7c@code.edu.az>
2024-03-12 13:29:35 +0400

    c# arrayla ishlemek

---
+++ arrayConsoleApp1/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace arrayConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Task1
+            //int[] arr ={12,24,35,46,57};
+            //int min = arr[0];
+            //for (int i = 0; i < arr.Length; i++)
+            //{
+            //  if(  arr[i] < min)
+            //    {
+            //        arr[i] = min;
+            //    }
+            //}
+            //Console.WriteLine(min);
+
+
+
+            //Task2
+            //String[] arr = { "Sema", "Ekber", "Oxu" };
+            //for (int i = 0; i < arr.Length; i++)
+            //{
+            //    string soz = arr[i];
+            //    for (int j = soz.Length - 1; j >= 0; j--)
+            //    {
+            //        Console.Write(soz[j]);
+            //    }
+            //    Console.WriteLine();
+            //    Console.WriteLine("bir eded daxil et:");
+            //    string input = Console.ReadLine();
+
+            //    int number;
+            //    bool success = int.TryParse(input, out number);
+            //    if (success)
+            //    {
+            //        Console.WriteLine("Daxil etdiyimiz eded:" + number);
+            //    }
+            //    else
+            //    {
+            //        Console.WriteLine("giris olmadi.");
+            //    }
+
+
+
+
+                //Task3
+                //int[] numbers = { -2, 3,4,-5 };
+                //int[] arr = new int[numbers.Length];
+                //for (int i = 0; i < arr.Length; i++)
+                //{
+                //    numbers[i] = Math.Abs(numbers[i]);
+                //    Console.WriteLine(numbers[i]+"");
+                //}
+                //for (int i = numbers.Length - 1; i >= 0; i--)
+                //{
+                //    arr[numbers.Length - 1 - i] = numbers[i];
+                //    Console.WriteLine(numbers[i]);
+                //}
+
+
+
+}
+
+
+            
+    }
+}
